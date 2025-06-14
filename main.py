e59269ea4fdb8c08804473bd4f7807da7587800c
ealcobaca <e.alcobaca@gmail.com>
2025-06-14 16:25:48 -0300

    Add database

--- main.py
+++ main.py
@@ -2,37 +2,40 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional, List
 
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+
 class ToDo(BaseModel):
     name: str
-    description: Optional[str] = None
-    author: Optional[str] = None
+    description: str | None = None
+    author: str | None = None
+    id: int | None
+
+
+class ToDoTable(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    name: str
+    description: str | None = None
+    author: str | None = None
+
+
+engine = create_engine(url="sqlite:///my-database")
+SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
-MEMORY: List[ToDo] = [ToDo(name="default")]
-
 @app.post("/todo/")
 async def create_item(todo: ToDo):
-    if todo.description is not None:
+    with Session(engine) as s:
         todo_dict = todo.model_dump()
-        todo_dict["description_len"] = len(todo.description)
-        MEMORY.append(ToDo(**todo_dict))
-        return todo_dict
-    else:
-        MEMORY.append(todo)
-        return todo
-
-
-@app.get("/todo/last")
-async def get_latest_item():
-    return MEMORY[-1]
-
-
-@app.get("/todo/first")
-async def get_latest_item():
-    return MEMORY[0]
+        s.add(ToDoTable(**todo_dict))
+        s.commit()
+    
+    return todo
 
 
 @app.get("/todo/all")
 async def get_latest_item():
-    return MEMORY
+    with Session(engine) as s:
+        stmt = select(ToDoTable)
+        result = s.exec(stmt)
+        return result.all()
